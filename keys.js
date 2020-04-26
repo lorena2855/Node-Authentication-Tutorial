02840d5a0ebf8b4a0cf5677b372ff1a78c5b8099
lorena2855 <61516883+lorena2855@users.noreply.github.com>
2020-04-26 01:05:51 -0500

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'remotemysql.com',
+        user: 'DLi87n6MIk',
+        password: 'WfSNpAss3f',
+        database: 'DLi87n6MIk'
+    }
+
+};
